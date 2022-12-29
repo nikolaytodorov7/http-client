33785dd3ac486b54064a7ab10942a33872d83181
Nikolay Todorov <105609346+nikolaytodorov7@users.noreply.github.com>
2022-12-29 16:01:50 +0200

    Clear

--- Http/src/main/java/org/example/http/impl/HttpClientBuilderImpl.java
+++
@@ -1,7 +0,0 @@
-package org.example.http.impl;
-
-public class HttpClientBuilderImpl {
-    public HttpClientImpl build() {
-        return new HttpClientImpl(this);
-    }
-}
--- Http/src/main/java/org/example/http/impl/RequestPublishers.java
+++
@@ -1,4 +0,0 @@
-package org.example.http.impl;
-
-public class RequestPublishers {
-}
